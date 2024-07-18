ceb8abc98771a172b2a72baaa980c78cba58be71
inesmariao <inesmao.misena@gmail.com>
2024-07-18 07:59:23 -0500

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { AppRouter } from './routing/AppRouter'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Ejercicio con Context API en React</h1>
+        <AppRouter />
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/routing/AppRouter.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../components/Home";
+import { Articles } from "../components/Articles";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      {/* Menú de Navegación */}
+
+
+      {/* Configurar rutas */}
+      <Routes>
+      </Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/inicio' element={<Home />}></Route>
+        <Route path='/articulos' element={<Articles />}></Route>
+        <Route path='*' element={
+          (
+            <div>
+              <h1>Esta es la página de error</h1>
+            </div>
+          )
+        }></Route>
+    </Router>
+  )
+}
